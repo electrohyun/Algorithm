c8fb703f06a9c98ff009487c7b18e8ab887cc42b
electrohyun <dev.electrohyun@gmail.com>
2026-07-27 11:45:15 +0900

    day15. 프로그래머스 그래프·DFS - 네트워크 개념학습 (문제 미완)

---
+++ graph/02_programmers-43162-network.js
@@ -0,0 +1,13 @@
+// BFS/DFS — 프로그래머스 네트워크 (43162)
+// https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+function solution(n, computers) {
+  let answer = 0;
+
+
+  return answer;
+}
+
+// 자기검증용
+// console.log(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])); // 기대값 2
+// console.log(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])); // 기대값 1
